c786c8084b63cb2d95aa1a3cf711239c6bd25c6c
uts.ultimateshop.fe <uts.ultimateshop.fe@proton.me>
2024-05-22 18:14:13 +0530

    order-domain-core event added

---
+++ order-service/order-domain/order-domain-core/src/main/kotlin/com/food/ordering/system/order/service/domain/event/OrderPaidEvent.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.order.service.domain.event;
+
+import com.food.ordering.system.domain.event.DomainEvent;
+import com.food.ordering.system.order.service.domain.entity.Order;
+
+public class OrderPaidEvent  implements DomainEvent<Order> {
+
+}
