472d072db0160597f1745f480bd6c93e26448876
uts.ultimateshop.fe <uts.ultimateshop.fe@proton.me>
2024-07-01 11:49:18 +0530

    OderDomainService completed

--- order-service/order-domain/order-domain-core/src/main/kotlin/com/food/ordering/system/order/service/domain/OrderDomainService.java
+++ order-service/order-domain/order-domain-core/src/main/kotlin/com/food/ordering/system/order/service/domain/OrderDomainService.java
@@ -2,6 +2,7 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.entity.Resturant;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 
@@ -15,7 +16,7 @@ public interface OrderDomainService {
 
     void approvedOrder(Order order);
 
-    OrderCreatedEvent cancelledOrderPayment(Order order, List<String> failureMessage);
+    OrderCancelledEvent cancelledOrderPayment(Order order, List<String> failureMessage);
 
     void cancelOrder(Order order,List<String> failureMessage);
 }
--- order-service/order-domain/order-domain-core/src/main/kotlin/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
+++ order-service/order-domain/order-domain-core/src/main/kotlin/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
@@ -1,23 +1,21 @@
 package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.entity.Product;
 import com.food.ordering.system.order.service.domain.entity.Resturant;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
 import lombok.extern.java.Log;
-import lombok.extern.log4j.Log4j;
-import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
-
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-@Slf4j
-@Log4j
 @Log
 public class OrderDomainServiceImpl implements  OrderDomainService{
 
+    private static final String UTC = "UTC";
 
     @Override
     public OrderCreatedEvent validateAndInitiateOrder(Order order, Resturant resturant) {
@@ -26,36 +24,51 @@ public class OrderDomainServiceImpl implements  OrderDomainService{
         order.validateOrder();
         order.initializeOrder();
         String orderId=order.getId().getValue().toString();
-//        log.info("Order with id: {} is initiated",orderId);
+        log.info("Order with id: "+order.getId().getValue()+" is initiated");
 
-        return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of("UTC")));
+        return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
     }
 
 
     @Override
     public OrderPaidEvent payOrder(Order order) {
-        return null;
+        order.pay();
+        log.info("Order with id "+order.getId().getValue()+" is paid");
+        return new OrderPaidEvent(order,ZonedDateTime.now(ZoneId.of(UTC)));
     }
 
     @Override
     public void approvedOrder(Order order) {
-
+        order.approve();
+        log.info("Order with id "+order.getId().getValue()+"is approved");
     }
 
     @Override
-    public OrderCreatedEvent cancelledOrderPayment(Order order, List<String> failureMessage) {
-        return null;
+    public OrderCancelledEvent cancelledOrderPayment(Order order, List<String> failureMessage) {
+        order.initCancel(failureMessage);
+        log.info("Order payment cancel for id "+order.getId().getValue());
+        return new OrderCancelledEvent(order,ZonedDateTime.now(ZoneId.of(UTC)));
     }
 
     @Override
     public void cancelOrder(Order order, List<String> failureMessage) {
-
+        order.cancel(failureMessage);
+        log.info("Order with id "+order.getId().getValue()+"is cancelled");
     }
 
     private void validateResturant(Resturant resturant) {
+        if(!resturant.isActive()){
+            throw new OrderDomainException("Resturant with id  "+resturant.getId().getValue()+" is  currently not active.");
+        }
     }
 
     private void setProductOrderInformation(Order order, Resturant resturant) {
+        order.getItems().forEach(orderItems -> resturant.getProducts().forEach(resturantProduct ->{
+            Product currentProduct = orderItems.getProduct();
+            if(currentProduct.equals(resturantProduct)){
+                currentProduct.updateWithConfirmedNameAndPrice(resturantProduct.getName(),resturantProduct.getPrice());
+            }
+        }));
     }
 
 }
