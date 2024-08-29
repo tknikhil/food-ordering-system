bc738ad42278cd608ca0e2085c74e24afec685ad
uts.ultimateshop.fe <uts.ultimateshop.fe@proton.me>
2024-08-29 17:14:29 +0530

    started making impl

---
+++ order-service/order-domain/order-application-service/src/main/kotlin/com/food/ordering/system/order/service/domain/OrderApplicationServiceImpl.java
@@ -0,0 +1,19 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.ports.inputs.service.OrderApplicationService;
+
+public class OrderApplicationServiceImpl implements OrderApplicationService {
+    @Override
+    public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand) {
+        return null;
+    }
+
+    @Override
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+        return null;
+    }
+}
